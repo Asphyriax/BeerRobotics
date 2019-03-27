0e53f398fe87c86ce81060eb545f229a34ada5dc
Asphyriax <N.ottens@outlook.com>
2019-03-27 17:51:50 +0100

    second test

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,13 +1,9 @@
 package sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+
 
 public class Main extends Application {
-
+/*
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -16,8 +12,8 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
+*/
     public static void main(String[] args) {
-        launch(args);
+       system.out.println("Hello world this is gonna be a beer project");
     }
 }
