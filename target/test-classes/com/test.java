7e9b9f789848312bf7a06c7546d1d28bc66c2670
asphyriax <asphyriax@gmail.com>
2019-03-28 16:16:16 +0100

    fix

---
+++ target/test-classes/com/test.java
@@ -0,0 +1,15 @@
+package com;
+
+public class test {
+    public static void main(String[] args) {
+
+
+        System.out.println("MAven is uncessery complicated");
+    }
+}
+
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
